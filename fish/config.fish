3a7b9b23cbfe3d98a0838bccf652ecc65f33293a
overk1ll <btaylor6192@gmail.com>
2016-12-09 08:14:40 -0600

    2nd commit

---
+++ fish/config.fish
@@ -0,0 +1,26 @@
+function bind_bang
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -t $history[1]; commandline -f repaint
+        case "*"
+            commandline -i !
+    end
+end
+
+function bind_dollar
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -t ""
+            commandline -f history-token-search-backward
+        case "*"
+            commandline -i '$'
+    end
+end
+
+function fish_user_key_bindings
+    bind ! bind_bang
+    bind '$' bind_dollar
+end
+
+set -gx CLICOLOR 1
+set -gx TERM xterm-256color
