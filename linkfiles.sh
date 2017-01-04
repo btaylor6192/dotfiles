34e50d9ac06a070311d203b9bd6ee1a801be0895
overk1ll <btaylor6192@gmail.com>
2017-01-04 15:15:15 -0600

    updated linkfiles paths

--- linkfiles.sh
+++ linkfiles.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 # This file will create symlinks to specified dotfiles
-ln -s ~/dotfiles/.Xresources ~/.Xresources
-ln -s ~/dotfiles/.Xdefaults ~/.Xdefaults
-ln -s ~/dotfiles/.bashrc ~/.bashrc
+ln -s ~/dotfiles/Xresources ~/.Xresources
+ln -s ~/dotfiles/Xdefaults ~/.Xdefaults
+ln -s ~/dotfiles/bashrc ~/.bashrc
 ln -s ~/dotfiles/.config/i3/config ~/.config/i3/config
 mkdir ~/.i3
 ln -s /home/blaine/dotfiles/.i3/i3status.conf /home/blaine/.i3/i3status.conf
