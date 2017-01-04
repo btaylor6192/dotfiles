1c78b6aa33461b986766527bfdd9f212866fb9ee
overk1ll <btaylor6192@gmail.com>
2017-01-04 15:42:44 -0600

    i3status updates

--- linkfiles.sh
+++ linkfiles.sh
@@ -4,6 +4,7 @@ ln -s ~/dotfiles/Xresources ~/.Xresources
 ln -s ~/dotfiles/Xdefaults ~/.Xdefaults
 ln -s ~/dotfiles/bashrc ~/.bashrc
 ln -s ~/dotfiles/config/i3/config ~/.config/i3/config
+ln -s ~/dotfiles/vimrc ~/.vimrc
 mkdir ~/.i3
-ln -s /home/blaine/dotfiles/.i3/i3status.conf /home/blaine/.i3/i3status.conf
+ln -s ~/dotfiles/i3status.conf ~/.i3status.conf
 
