6f6d729158f62da88bb2a6794f406c0eb5d55c3a
overk1ll <btaylor6192@gmail.com>
2017-03-02 16:09:26 -0600

    removed Xdefaults

--- linkfiles.sh
+++ linkfiles.sh
@@ -2,7 +2,6 @@
 # This file will create symlinks to specified dotfiles
 
 ln -s ~/dotfiles/Xresources ~/.Xresources
-#ln -s ~/dotfiles/Xdefaults ~/.Xdefaults
 ln -s ~/dotfiles/bashrc ~/.bashrc
 ln -s ~/dotfiles/config/i3/config ~/.config/i3/config
 ln -s ~/dotfiles/vimrc ~/.vimrc
