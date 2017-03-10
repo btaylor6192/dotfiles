c90c6c8aa6f2a707f687e73048ccd2ac75626887
overk1ll <btaylor6192@gmail.com>
2017-03-10 13:25:22 -0600

    dircolors rename, linkfiles update to backup current files

--- linkfiles.sh
+++ linkfiles.sh
@@ -1,10 +1,30 @@
 #! /bin/bash
 # This file will create symlinks to specified dotfiles
 
+echo 'Backing up current config files...'
+
+mv ~/.Xresources ~/.Xresources.bak
+mv ~/.bashrc ~/.bashrc.bak
+mv ~/.config/i3/config ~/.config/i3/config.bak
+mv ~/.vimrc ~/.vimrc.bak
+mv ~/.i3/i3status.conf ~/.i3/i3status.conf.bak
+
+echo 'Linking new config files...'
+ 
 ln -s ~/dotfiles/Xresources ~/.Xresources
 ln -s ~/dotfiles/bashrc ~/.bashrc
 ln -s ~/dotfiles/config/i3/config ~/.config/i3/config
 ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/i3status.conf ~/.i3/i3status.conf
-ln -s ~/dotfiles/config.fish ~/.config/fish/config.fish
+ln -s ~/dotfiles/dir_colors ~/.dir_colors
+#ln -s ~/dotfiles/config.fish ~/.config/fish/config.fish
+
+echo 'Loading Xresources'
+xrdb -load ~/.Xresources
+
+echo 'Setting wallpaper'
+feh --bg-scale ~/dotfiles/wallpapers/sky-clouds.jpg
+
+echo "Don't forget to reload i3!"
+
 
