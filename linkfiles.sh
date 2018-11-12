a50a70ed1e205489dd32f551813c3ee282e6a523
overk1ll <btaylor6192@gmail.com>
2018-11-12 11:34:46 -0600

    dell laptop update

--- linkfiles.sh
+++ linkfiles.sh
@@ -4,24 +4,24 @@ WALLPAPER=~/dotfiles/wallpapers/low_poly_planet.png
 
 echo 'Removing current links...'
 
-rm ~/.Xresources 
-rm ~/.bashrc 
+#rm ~/.Xresources 
+#rm ~/.bashrc 
 rm ~/.config/i3/config 
-rm ~/.vimrc 
+#rm ~/.vimrc 
 rm ~/.i3/i3status.conf
-rm ~/.archey3.cfg
-rm ~/.dir_colors
+#rm ~/.archey3.cfg
+#rm ~/.dir_colors
 
 echo 'Linking new config files...'
  
-ln -s ~/projects/dotfiles/Xresources ~/.Xresources
-ln -s ~/projects/dotfiles/bashrc ~/.bashrc
+#ln -s ~/projects/dotfiles/Xresources ~/.Xresources
+#ln -s ~/projects/dotfiles/bashrc ~/.bashrc
 ln -s ~/projects/dotfiles/i3/config ~/.config/i3/config
-ln -s ~/projects/dotfiles/vimrc ~/.vimrc
-# TODO: add logic for i3status option for different profiles
+#ln -s ~/projects/dotfiles/vimrc ~/.vimrc
+## TODO: add logic for i3status option for different profiles
 ln -s ~/projects/dotfiles/i3status-lenovo ~/.i3/i3status.conf
-ln -s ~/projects/dotfiles/dir_colors ~/.dir_colors
-ln -s ~/projects/dotfiles/archey3.cfg ~/.archey3.cfg
+#ln -s ~/projects/dotfiles/dir_colors ~/.dir_colors
+#ln -s ~/projects/dotfiles/archey3.cfg ~/.archey3.cfg
 #ln -s ~/dotfiles/config.fish ~/.config/fish/config.fish
 
 echo 'Loading Xresources'
